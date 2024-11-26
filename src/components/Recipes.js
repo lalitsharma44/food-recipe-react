c7a3942788bbf0fbb06237b3cd9ad8c8c17277b0
lalitsharma44 <ls709161@gmail.com>
2024-11-26 18:06:24 +0530

    add msg for no racipes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,17 +5,25 @@ const Recipes = props => {
 	const { recipes } = props;
 
 	return (
-		<div class="row">
-			{recipes.map((recipe, index) => (
-				<div className="col" key={index}>
-					<div className="card py-2">
-						<img src={recipe.recipe.image} className="card-img" alt={recipe.recipe.label} />
-						<div className="card-body">
-							<h5>{recipe.recipe.label}</h5>
-						</div>
+		<div>
+			{
+				recipes.length === 0 ? (
+					<h1 style={{textAlign:'center'}}>No Recipes</h1>
+				) : (
+					<div class="row">
+						{recipes.map((recipe, index) => (
+							<div className="col" key={index}>
+								<div className="card py-2">
+									<img src={recipe.recipe.image} className="card-img" alt={recipe.recipe.label} />
+									<div className="card-body">
+										<h5>{recipe.recipe.label}</h5>
+									</div>
+								</div>
+							</div>
+						))}
 					</div>
-				</div>
-			))}
+				)
+			}
 		</div>
 
 	);
